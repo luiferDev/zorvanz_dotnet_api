b48e190af94122abe29534093a432ec56f72a2b0
luifer991 <luifer991@gmail.com>
2025-05-26 23:35:34 -0500

    createProductsDto

---
+++ zorvanz-api/Models/DTO/CreateProductDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace zorvanz_api.Models.DTO;
+
+public class CreateProductDto
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public decimal Price { get; set; }
+    [Required]
+    public int CategoryId { get; set; }
+    public required string ImageUrl { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
+    public double? popularity { get; set; }
+}
