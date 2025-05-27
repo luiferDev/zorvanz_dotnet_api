270e866bd35271c04722a668068fee457bc146d7
luifer991 <luifer991@gmail.com>
2025-05-26 20:48:31 -0500

    add connection string and add to Program.cs file

---
+++ zorvanz-api/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using zorvanz_api.ZorvanzDbContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Cargar conexión a base de datos
+builder.Services.AddDbContext<ZorvanzContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ZorvanzContext")
+                         ?? throw new InvalidOperationException("Connection string 'ZorvanzContext' not found."));
+});
+
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "https://zorvanz.vercel.app") // Especificar dominios permitidos
+            .AllowAnyHeader() // Permitir cualquier encabezado
+            .AllowAnyMethod() // Permitir cualquier método HTTP (GET, POST, etc.)
+            .AllowCredentials(); // Permitir cookies o credenciales
+    });
+
+    /*options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() // Permitir cualquier dominio
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });*/
+});
+
+var app = builder.Build();
+
+// Aplicar CORS (antes de los controladores)
+app.UseCors("AllowSpecificOrigins"); // O "AllowAll" según tu caso
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Energías Renovables v1");
+        c.RoutePrefix = string.Empty; // Para que Swagger esté en la raíz del proyecto
+    });
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
