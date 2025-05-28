e8fa8843bbc1e2db0707be89e6d084234c20f9d6
luifer991 <luifer991@gmail.com>
2025-05-28 10:58:13 -0500

    add UserRepository.cs

---
+++ zorvanz-api/Services/UserRepository.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using zorvanz_api.Models;
+using zorvanz_api.ZorvanzDbContext;
+
+namespace zorvanz_api.Services;
+
+public class UserRepository(ZorvanzContext context)
+{
+    public async Task<User?> GetByUsernameAsync(string? username)
+    {
+        return await context.Users.FirstOrDefaultAsync(u => u.UserName == username);
+    }
+
+    public async Task AddUserAsync(User user)
+    {
+        await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+    }
+}
