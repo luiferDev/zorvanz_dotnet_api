4f8f10e577cc5d50f48ec87a88f0ec6dc001e326
luifer991 <luifer991@gmail.com>
2025-05-28 10:49:39 -0500

    Add LoginController.cs to manage user's login TODO: create an update and delete methods

---
+++ zorvanz-api/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using zorvanz_api.Models.DTO.User;
+using zorvanz_api.Services;
+
+namespace zorvanz_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController(AuthService authService): ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> LoginUser([FromBody] LoginUserDto login)
+    {
+        var token = await authService.LoginUserAsync(login.UserName, login.Password);
+        if (token == null)
+            return Unauthorized(new { Message = "Credenciales inválidas." });
+
+        return Ok(new { Token = token });
+    }
+}
