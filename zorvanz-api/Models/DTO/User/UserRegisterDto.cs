48a7a0aa3ef9b0dc91581694a4603dfd9ddfb181
luifer991 <luifer991@gmail.com>
2025-05-28 10:58:03 -0500

    add UserRegisterDto.cs

---
+++ zorvanz-api/Models/DTO/User/UserRegisterDto.cs
@@ -0,0 +1,10 @@
+namespace zorvanz_api.Models.DTO.User;
+
+public class UserRegisterDto
+{
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+}
