7376be41dd9a12daf4a0f8c656d7b2bc21183102
luifer991 <luifer991@gmail.com>
2025-05-28 10:53:51 -0500

    add swagger documentation

--- zorvanz-api/Program.cs
+++ zorvanz-api/Program.cs
@@ -11,6 +11,67 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// Configurar servicios de Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API Zorvanz",
+        Version = "v1",
+        Description = "API para gestionar usuarios con autenticación JWT."
+    });
+
+    // Configurar el esquema de autenticación Bearer
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Ingrese el token JWT en este formato: Bearer {token}"
+    });
+
+    // Requerir el esquema en todos los endpoints protegidos
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+// Configurar JWT Authentication
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"] ?? throw new InvalidOperationException());
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(opt =>
+{
+    var signingkey = new SymmetricSecurityKey(key);
+    var signingCredential = new SigningCredentials(signingkey, SecurityAlgorithms.HmacSha256Signature);
+
+    opt.RequireHttpsMetadata = false;
+
+    opt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        IssuerSigningKey = signingkey
+    };
+});
+
+
+
 // Cargar conexión a base de datos
 builder.Services.AddDbContext<ZorvanzContext>(options =>
 {
