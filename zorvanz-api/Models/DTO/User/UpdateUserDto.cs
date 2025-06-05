2fad6f1935d5956523beccef33abd09e2972c04b
luifer991 <luifer991@gmail.com>
2025-06-04 22:13:15 -0500

    add: update user dto

---
+++ zorvanz-api/Models/DTO/User/UpdateUserDto.cs
@@ -0,0 +1,9 @@
+namespace zorvanz_api.Models.DTO.User;
+
+public class UpdateUserDto
+{
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+}
